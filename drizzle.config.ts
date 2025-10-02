0034a024ec588e47d6124c6e384d457f47b11641
iamVihanga <vihangarashansilva@gmail.com>
2025-10-02 14:54:37 +0530

    Fix Issue : Deployment Issues on Vercel - Chart Component error & Typesafe Env errors #4

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "drizzle-kit";
 
-import { env } from "@/lib/config/env";
-
 export default defineConfig({
   schema: "./src/database/schema/index.ts",
   out: "./src/database/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL
+    url: process.env.DATABASE_URL!
   },
   strict: true
 });
--- src/lib/rpc/base_instance.ts
+++ src/lib/rpc/base_instance.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { hc } from "hono/client";
 
-import { app } from "@/app/(backend)/api/[[...route]]/route";
+import { app } from "@/app/(backend)/routes/registry";
 import { env } from "@/lib/config/env";
 
 type AppType = typeof app;
 
-const client = hc<AppType>(env.NEXT_PUBLIC_CLIENT_APP_URL, {
+const client = hc<AppType>(env.NEXT_PUBLIC_CLIENT_APP_URL!, {
   fetch: ((input, init) => {
     return fetch(input, {
       ...init,
