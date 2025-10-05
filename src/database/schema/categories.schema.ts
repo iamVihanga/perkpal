7711c6b71e2a0533b593efb89e50bc5d05953f6c
iamVihanga <vihangarashansilva@gmail.com>
2025-10-05 10:51:26 +0530

    Add: Categories database schema

---
+++ src/database/schema/categories.schema.ts
@@ -0,0 +1,96 @@
+import { timestamps } from "@/lib/helpers";
+import { relations, sql } from "drizzle-orm";
+import {
+  check,
+  index,
+  integer,
+  pgTable,
+  text,
+  unique,
+  uniqueIndex
+} from "drizzle-orm/pg-core";
+import { media } from "./media.schema";
+
+export const categories = pgTable(
+  "categories",
+  {
+    id: text("id")
+      .primaryKey()
+      .default(sql`gen_random_uuid()`),
+
+    name: text("name").notNull(),
+    slug: text("slug").unique().notNull(),
+    description: text("description"),
+
+    displayOrder: integer("display_order").default(0),
+
+    // SEO Fields
+    seoTitle: text("seo_title"),
+    seoDescription: text("seo_description"),
+    og_image_id: text("og_image_id").references(() => media.id, {
+      onDelete: "set null"
+    }),
+
+    ...timestamps
+  },
+  (t) => [
+    uniqueIndex("categories_slug_idx").on(t.slug),
+    index("categories_display_order_idx").on(t.displayOrder),
+    index("categories_name_idx").on(t.name),
+    index("categories_id_idx").on(t.id),
+
+    // Ensure display order is positive
+    check("categories_display_order_check", sql`${t.displayOrder} > 0`)
+  ]
+);
+
+export const subcategories = pgTable(
+  "subcategories",
+  {
+    // PK
+    id: text("id")
+      .primaryKey()
+      .default(sql`gen_random_uuid()`),
+
+    // FK
+    categoryId: text("category_id").references(() => categories.id, {
+      onDelete: "cascade"
+    }),
+
+    name: text("name").notNull(),
+    slug: text("slug").unique().notNull(),
+    description: text("description"),
+
+    displayOrder: integer("display_order").default(0),
+
+    // SEO Fields
+    seoTitle: text("seo_title"),
+    seoDescription: text("seo_description"),
+    og_image_id: text("og_image_id").references(() => media.id, {
+      onDelete: "set null"
+    }),
+
+    ...timestamps
+  },
+  (t) => [
+    unique().on(t.categoryId, t.slug),
+    index("subcategories_category_id_idx").on(t.categoryId),
+    index("subcategories_display_order_idx").on(t.displayOrder),
+    index("subcategories_name_idx").on(t.name),
+
+    // Ensure display order is positive
+    check("categories_display_order_check", sql`${t.displayOrder} > 0`)
+  ]
+);
+
+// Relationships
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  subcategories: many(subcategories)
+}));
+
+export const subcategoriesRelations = relations(subcategories, ({ one }) => ({
+  category: one(categories, {
+    fields: [subcategories.categoryId],
+    references: [categories.id]
+  })
+}));
--- src/database/schema/index.ts
+++ src/database/schema/index.ts
@@ -3,3 +3,5 @@ export * from "./auth.schema";
 export * from "./tasks.schema";
 
 export * from "./media.schema";
+
+export * from "./categories.schema";
